6ee84d61d7701900c8f6726e7cb550480e023d3a
Yaksh Darji <95200752+dyaksh@users.noreply.github.com>
2024-04-16 19:52:32 +0530

    about solved

--- pages/about/index.js
+++ pages/about/index.js
@@ -88,11 +88,12 @@ const aboutData = [
     ],
   },
 ];
+
 import Avatar from '../../components/Avatar';
 import Circles from '../../components/Circles';
 import { motion } from 'framer-motion';
 import { fadeIn } from '../../variants';
-import CountUp from 'react-countup'
+import CountUp from 'react-countup';
 
 const About = () => {
   const [index, setIndex] = useState(0);
@@ -101,137 +102,132 @@ const About = () => {
   return (
     <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
       <Circles />
-    {/* Avatar */}
-    <motion.div
-      variants={fadeIn("right", 0.2)}
-      initial="hidden"
-      animate="show"
-      exit="hidden"
-      className="hidden xl:flex absolute bottom-0 -left-[370px]"
-    >
-      <Avatar />
-    </motion.div>
-  
-      <div className='container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6'>
-        {/* Text */}
-        <div className="flex-1 flex flex-col justify-center">
-          <motion.h2 
-          className="max-w-[500px] mx-auto x:mx-0 mb-6 xl:mb-12 px-2 xl:px-0 h2"
-          exit='hidden'
-          initial='hidden'
-          animate='show'
-          variants={fadeIn('right',0.2)}
-          >
-            Captivating 
-          <span className="text-accent"> stories </span> birth  magnificant  designs.
-          </motion.h2>
-
-          <motion.p
-          exit='hidden'
-          initial='hidden'
-          animate='show'
-          variants={fadeIn('right',0.4)}
-          className="max-w-[500px] mx-auto x:mx-0 mb-6 xl:mb-12 px-2 xl:px-0"
-          >10 years ago , I began freelancing as developer .Sincle then I've done remote work for agencies, consulted for startups , and collaborated on digital products for business and consumer use.
-          </motion.p>
-          {/*counter*/}
-          <br></br>
-        <motion.div  
-          exit='hidden'
-          initial='hidden'
-          animate='show'
-          variants={fadeIn('right',0.6)}
-          className="hidden md:flex md:max-w-xl x:max-w-none mx-auto  xl:max-0 mb-8">
-            <div className="flex flex-2 xl:gap-x-8 ">
-                {/* Experience */}
-                <div className="relative flex-1 after:w-[1px] after:h-full 
-                after:bg-white/10 after:absolute after:top-0 after:right-0">
-                  <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2 ">
-                    <CountUp start={0} end={10} duration={5} /> +
-                  </div>
-                  <div className="text-xs uppercase  tracking-[1px]  leading-[1.4] max-w-[100px]">Years of Experience</div>
-                </div>
-                {/* Clients */}
-                <div className="relative flex-1 after:w-[1px] after:h-full 
-                after:bg-white/10 after:absolute after:top-0 after:right-0">
-                  <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2 ">
-                    <CountUp start={0} end={250} duration={5} />+
-                  </div>
-                  <div className="text-xs uppercase  tracking-[1px]  leading-[1.4] max-w-[100px]">
-                    Satisified Client
-                  </div>
-                </div>
-                {/* project */}
-                <div className="relative flex-1 after:w-[1px] after:h-full 
-                after:bg-white/10 after:absolute after:top-0 after:right-0">
-                  <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2 ">
-                    <CountUp start={0} end={650} duration={5} />+
-                  </div>
-                  <div className="text-xs uppercase  tracking-[1px]  leading-[1.4] max-w-[100px]">YFinished Projects</div>
-                </div>
-                {/* Awatds */}
-                <div className="relative flex-1 after:w-[1px] after:h-full 
-                after:bg-white/10 after:absolute after:top-0 after:right-0">
-                  <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2 ">
-                    <CountUp start={0} end={8} duration={5} />+
-                  </div>
-                  <div className="text-xs uppercase  tracking-[1px]  leading-[1.4] max-w-[100px]">Winning Awards</div>
-                </div>
-          </div>
-        </motion.div>
-        </div>
-
-
-
-        {/* info */}
-        <motion.div 
-        variants={fadeIn("left", 0.4)}
+      {/* Avatar */}
+      <motion.div
+        variants={fadeIn("right", 0.2)}
         initial="hidden"
         animate="show"
         exit="hidden"
-        className="flex flex-col w-full xl:max-w-[48%] h-[480px]">
-        <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
-          {aboutData.map((item, itemIndex) => {
-            return (
-              <div
-              key={itemIndex}
-              className={`${
-                index === itemIndex &&
-                'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'
-              } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px]
-              after:bg-white after:absolute after:-bottom-1 after:left-0`}
-              onClick={() => setIndex(itemIndex)}
-            >
-              {item.title}
-            </div>
-            
-            
+        className="hidden xl:flex absolute bottom-0 -left-[370px]"
+      >
+        <Avatar />
+      </motion.div>
 
-                
-            );
-          })}
-          </div>
-        <div className="py-2 xl:py-6  flex flex-col gap-y-2 xl:gap-y-4  items-center xl:items-start ">
-          {aboutData[index].info.map((item , itemIndex )=>{
-            return(
-              <div
-              key={itemIndex}
-              className="flex-1 flex flex-col  md:flex-row max-w-max  gap-x-2  items-center text-white/60"
-              >
-                <div className="font-light mb-2 md:mb-0">{item.title}</div>
-                <div className="hidden md:flex">-</div>
-                <div>{item.stage}</div>
-                <div className="flex gap-x-4">
-                  {/* icons*/}
-                  {item.icons?.map((icon ,itemIndex) => {
-                    return( <div className="text-2xl text-white">{icon}</div> );
-                  })}
+      <div className='container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6'>
+        {/* Text */}
+        <div className="flex-1 flex flex-col justify-center">
+          <motion.h2
+            className="max-w-[500px] mx-auto x:mx-0 mb-6 xl:mb-12 px-2 xl:px-0 h2"
+            exit='hidden'
+            initial='hidden'
+            animate='show'
+            variants={fadeIn('right', 0.2)}
+          >
+            Captivating
+            <span className="text-accent"> stories </span> birth  magnificant  designs.
+          </motion.h2>
+
+          <motion.p
+            exit='hidden'
+            initial='hidden'
+            animate='show'
+            variants={fadeIn('right', 0.4)}
+            className="max-w-[500px] mx-auto x:mx-0 mb-6 xl:mb-12 px-2 xl:px-0"
+          >
+            10 years ago, I began freelancing as developer. Since then I&apos;ve done remote work for agencies, consulted for startups, and collaborated on digital products for business and consumer use.
+          </motion.p>
+          {/* counter */}
+          <br></br>
+          <motion.div
+            exit='hidden'
+            initial='hidden'
+            animate='show'
+            variants={fadeIn('right', 0.6)}
+            className="hidden md:flex md:max-w-xl x:max-w-none mx-auto  xl:max-0 mb-8">
+            <div className="flex flex-2 xl:gap-x-8 ">
+              {/* Experience */}
+              <div className="relative flex-1 after:w-[1px] after:h-full
+                after:bg-white/10 after:absolute after:top-0 after:right-0">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2 ">
+                  <CountUp start={0} end={10} duration={5} /> +
+                </div>
+                <div className="text-xs uppercase  tracking-[1px]  leading-[1.4] max-w-[100px]">Years of Experience</div>
+              </div>
+              {/* Clients */}
+              <div className="relative flex-1 after:w-[1px] after:h-full
+                after:bg-white/10 after:absolute after:top-0 after:right-0">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2 ">
+                  <CountUp start={0} end={250} duration={5} />+
+                </div>
+                <div className="text-xs uppercase  tracking-[1px]  leading-[1.4] max-w-[100px]">
+                  Satisified Client
                 </div>
               </div>
-            );
-          })}
+              {/* project */}
+              <div className="relative flex-1 after:w-[1px] after:h-full
+                after:bg-white/10 after:absolute after:top-0 after:right-0">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2 ">
+                  <CountUp start={0} end={650} duration={5} />+
+                </div>
+                <div className="text-xs uppercase  tracking-[1px]  leading-[1.4] max-w-[100px]">YFinished Projects</div>
+              </div>
+              {/* Awards */}
+              <div className="relative flex-1 after:w-[1px] after:h-full
+                after:bg-white/10 after:absolute after:top-0 after:right-0">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2 ">
+                  <CountUp start={0} end={8} duration={5} />+
+                </div>
+                <div className="text-xs uppercase  tracking-[1px]  leading-[1.4] max-w-[100px]">Winning Awards</div>
+              </div>
+            </div>
+          </motion.div>
         </div>
-        
+
+        {/* info */}
+        <motion.div
+          variants={fadeIn("left", 0.4)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          className="flex flex-col w-full xl:max-w-[48%] h-[480px]">
+          <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
+            {aboutData.map((item, itemIndex) => {
+              return (
+                <div
+                  key={itemIndex}
+                  className={`${
+                    index === itemIndex &&
+                    'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'
+                  } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px]
+              after:bg-white after:absolute after:-bottom-1 after:left-0`}
+                  onClick={() => setIndex(itemIndex)}
+                >
+                  {item.title}
+                </div>
+              );
+            })}
+          </div>
+          <div className="py-2 xl:py-6  flex flex-col gap-y-2 xl:gap-y-4  items-center xl:items-start ">
+            {aboutData[index].info.map((item, itemIndex) => {
+              return (
+                <div
+                  key={itemIndex}
+                  className="flex-1 flex flex-col  md:flex-row max-w-max  gap-x-2  items-center text-white/60"
+                >
+                  <div className="font-light mb-2 md:mb-0">{item.title}</div>
+                  <div className="hidden md:flex">-</div>
+                  <div>{item.stage}</div>
+                  <div className="flex gap-x-4">
+                    {/* icons */}
+                    {item.icons?.map((icon, itemIndex) => {
+                      return (<div className="text-2xl text-white">{icon}</div>);
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+
         </motion.div>
       </div>
     </div>
